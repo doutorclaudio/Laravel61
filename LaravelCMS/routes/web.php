be49b7a58cf369e6151e2bf4cb3bfbb9e787bd46
Luís Cláudio Júnior Ferreira <doutorclaudio@hotmail.com>
2020-04-17 10:36:51 -0300

    Atualizando pastas e arquivos

--- LaravelCMS/routes/web.php
+++ LaravelCMS/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'Site\HomeController@index');
+
+Route::prefix('painel')->group(function(){
+    Route::get('/', 'Admin\HomeController@index')->name('admin');
+    
+    Route::get('login', 'Admin\Auth\LoginController@index')->name('login');
+    Route::post('login', 'Admin\Auth\LoginController@authenticate');
+
+    Route::get('register', 'Admin\Auth\RegisterController@index')->name('register');
+    Route::post('register', 'Admin\Auth\RegisterController@register');
+
+    Route::post('logout', 'Admin\Auth\LoginController@logout')->name('logout');
 });
