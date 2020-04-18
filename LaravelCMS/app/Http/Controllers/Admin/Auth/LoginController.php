d7392228e19a50e67ad8e14c6f8338b6374b3ccc
Luís Cláudio Júnior Ferreira <doutorclaudio@hotmail.com>
2020-04-18 02:00:19 -0300

    Atualizando pastas e arquivos

--- LaravelCMS/app/Http/Controllers/Admin/Auth/LoginController.php
+++ LaravelCMS/app/Http/Controllers/Admin/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use illuminate\http\Request;
 
 class LoginController extends Controller
 {
@@ -43,12 +45,41 @@ class LoginController extends Controller
         return view('admin.login');
     }
 
-    public function auhenticate(){
+    public function authenticate(Request $request){
+        $data = $request->only([
+            'email',
+            'password'
+        ]);
+        $validator = $this->validator($data);
 
+        $remember = $request->input('remember', false);
+
+        if($validator->fails()){
+            return redirect()->route('login')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        if(Auth::attempt($data, $remember)){
+            return redirect()->route('admin');
+        }else{
+            $validator->errors()->add('password', 'E-mail e/ou senha errados!');
+
+            return redirect()->route('login')
+                ->withErrors($validator)
+                ->withInput();
+        }
     }
 
     public function logout(){
         Auth::logout();
         return redirect()->route('login');
+    }
+
+    protected function validator(array $data){
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:100'],
+            'password' => ['required', 'string', 'min:4']
+        ]);
     }
 }
--- LaravelCMS/config/adminlte.php
+++ LaravelCMS/config/adminlte.php
@@ -167,7 +167,7 @@ return [
 
     'use_route_url' => false,
 
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'painel',
 
     'logout_url' => 'painel/logout',
 
@@ -209,83 +209,31 @@ return [
 
     'menu' => [
         [
-            'text' => 'search',
-            'search' => true,
-            'topnav' => true,
-        ],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
+            'text'        => 'Dashboard',
+            'url'         => 'painel',
             'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
         ],
-        ['header' => 'account_settings'],
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
+            'text'        => 'Minhas Páginas',
+            'url'         => 'painel/pages',
+            'icon'        => 'far fa-fw fa-file',
+        ],
+        [
+            'text'        => 'Meus Usuários',
+            'url'         => 'painel/users',
+            'icon'        => 'far fa-fw fa-file',
+            'can'         => 'edit-users'
+        ],
+        ['header' => 'Configurações'],
+        [
+            'text' => 'Configurações do Site',
+            'url'  => 'painel/settings',
+            'icon' => 'fas fa-fw fa-file',
+        ],        
+        [
+            'text' => 'Meu Perfil',
+            'url'  => 'painel/profile',
             'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'aqua',
         ],
     ],
 
